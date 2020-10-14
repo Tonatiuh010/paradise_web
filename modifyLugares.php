0054313a9c8a56c74efe315ee2fab85051021c47
Tonatiuh010 <l.tonatiuh010@gmail.com>
2020-10-13 18:09:58 -0700

    Main Ooptions into Git

---
+++ modifyLugares.php
@@ -0,0 +1,81 @@
+<html>
+<body>
+
+<?php
+    $servername="localhost";
+$username="root";
+$password="losseisbastardos12";
+$dbname="paradise";
+// Credentials
+
+
+$conn= new mysqli($servername	,$username	,$password	,$dbname);
+
+if ($conn->connect_error){
+	die("Conexión Fallida: ".$conn->connect_error);
+    exit();
+}
+
+$sql="select * from salon where salNombre like '".$_GET['bar']."%';";
+$result=$conn->query($sql);
+
+if ($result->num_rows>0)
+{
+
+    while ($row=$result->fetch_assoc()){
+
+    $ob_lugar='{ "nombre":"'.$row['salNombre'].'", 
+               "desc":"'.$row['salDesc'].'", 
+               "costo":'.$row['salCosto'].',
+               "capacidad":'.$row['salCapacidad'].', 
+               "numero":'.$row['salNum'].'}';
+    
+    echo "
+    <table style='width:22%;margin-right:8%; border:2px solid;margin-bottom:3%;'>
+        <tr>
+            <td>Numero del Lugar </td>
+            <td>".$row['salNum']." </td>
+        </tr>
+
+        <tr>
+            <td>Nombre del Lugar </td>
+            <td>".$row['salNombre']." </td>
+        </tr>
+
+        <tr>
+            <td>Descripci&oacute;n del Lugar </td>
+            <td>".$row['salDesc']." </td>
+        </tr>
+
+        <tr>
+            <td>Costo del Lugar </td>
+            <td>".$row['salCosto']." </td>
+        </tr>
+
+
+        <tr>
+            <td>Capacidad del Lugar </td>
+            <td>".$row['salCapacidad']." </td>
+        </tr>
+    </table >
+    <button onclick='modifyLugar(".$ob_lugar.")'>click</button>
+    ";
+
+
+
+    }
+} else {
+    echo "No data founded";
+
+    echo "Sentence:".$sql;
+}
+
+
+
+
+?>
+
+
+
+</body>
+</html>
