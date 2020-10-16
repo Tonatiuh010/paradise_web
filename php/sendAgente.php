a776f2a76f62840533f952f006d409b2bf405b03
Tonatiuh010 <l.tonatiuh010@gmail.com>
2020-10-15 22:44:58 -0700

    Inicio de interfaz de agentes

---
+++ php/sendAgente.php
@@ -0,0 +1,35 @@
+<?php
+    $servername="localhost";
+    $username="root";
+    $password="losseisbastardos12";
+    $dbname="paradise";
+    // Credentials
+
+    $conn= new mysqli($servername	,$username	,$password	,$dbname);
+
+    if ($conn->connect_error){
+	    die("Conexión Fallida: ".$conn->connect_error);
+        exit();
+    }
+
+
+    $ob_agente=$_GET['b'];
+
+    echo $ob_agente->nombre;
+
+    //$sql="insert into salon (salNombre,salDesc,salCosto,salCapacidad) 
+    //values ('".$_GET['nombre']."','".$_GET['desc']."',".$_GET['costo'].",".$_GET['capacidad'].");";//SQL Sentence
+
+
+
+    //if ($conn->query($sql)===TRUE){
+
+    //    echo "Registrado!";
+
+    //} else {
+
+    //    echo "Error al momento de registrar:".$sql."-->".$conn->error;
+    //}
+
+
+?>
