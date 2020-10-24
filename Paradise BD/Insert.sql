9e224a4c232ba0f2a2a1edec2db96ebd2e199b80
Denisse-Arreola <arxie.denisse@gmail.com>
2020-10-23 17:39:43 -0700

    Rediseño de la BD

--- Paradise BD/Insert.sql
+++ Paradise BD/Insert.sql
@@ -1,69 +1,163 @@
 use paradise;
 
-insert into genero values ('M', 'Masculino'),('F','Femenino');
+# insert into genero values ('M', 'Masculino'),('F','Femenino');
 
-insert into tipo_us values ('CLI','Cliente'),('ADM','Administrador'),('AGT','Agente');
+# insert into tipo_us values ('CLI','Cliente'),('ADM','Administrador'),('AGT','Agente');
 
 insert into municipio values ('ESE','Ensenada'),('MXL','Mexicali'),('TIJ','Tijuana'),('ROS','Rosarito'),('TEC','Tecate');
 
 insert into espacio(espNombre) values
 ('Salón'),('Pista de baile'),('Estacionamiento'),('Cocina para catering'),('Terraza'),('Jardín'),('Carpa'),('Piscina'),('Jacuzzi');
 
-insert into metodopag(pm_Metodo) values('Efectivo'),('Tarjeta bancaria'),('Paypal'),('Tranferencia bancaria');
+# insert into metodopag(pm_Metodo) values('Efectivo'),('Tarjeta bancaria'),('Paypal'),('Tranferencia bancaria');
 
 insert into tipolugar(tlNombre) values
 ('Salón'),('Hacienda'),('Restaurante'),('Centro nocturno'),('Jardín'),('Quinta'),('Playa');
+
+select * from tipolugar;
 
 
 ########################################### AGENTES ###################################################################
 # call SP_insert_agente('Chapis','Pepogi','Ramirez','2000-12-23','F');
 
-call SP_insert_agente ('ANA CAROLINA','LOPEZ','MARTINEZ', '2000-12-23','F',null,null,null,null);
-call SP_insert_agente ('MANUEL','MENDEZ','HERNANDEZ','1990-08-03','M',null,null,null,null);
-call SP_insert_agente ('MARCO ANTONIO','RAMIREZ','LOZANO','1980-01-12','M',null,null,null,null);
-call SP_insert_agente ('JORGE','CARRILLO','MARTINEZ','1990-08-05','M',null,null,null,null);
-call SP_insert_agente ('JOHANA VANESSA','MAR','SANCHEZ','1989-07-21','F',null,null,null,null);
-call SP_insert_agente ('JESUS ENRIQUE','VAZQUEZ','MARIN','2000-12-23','M',null,null,null,null);
-call SP_insert_agente ('GINA ELIZABETH','LISZT','CORDOBA','1995-10-03','F',null,null,null,null);
-call SP_insert_agente ('PAOLA','MARTINEZ','LARA','1980-01-12','F',null,null,null,null);
-call SP_insert_agente ('MARTHA','CHAVEZ','OCHOA','1989-07-21','F',null,null,null,null);
-call SP_insert_agente ('JOSE ALFONSO','PEREZ','CANTO','1990-08-05','M',null,null,null,null);
-call SP_insert_agente ('FELIPE','LOPEZ','CHAVIRA','1999-07-21','M',null,null,null,null);
-call SP_insert_agente ('VERONICA','FERNANDEZ','ZAMUDIO','2000-12-23','F',null,null,null,null);
-call SP_insert_agente ('SANDRA','GARCIA','BUSTAMANTE','1989-07-21','F',null,null,null,null);
-call SP_insert_agente ('JUAN CARLOS','JARAMILLO','VAZQUEZ','1980-01-12','M',null,null,null,null);
-call SP_insert_agente ('VICTOR ALFONSO','CALDERON','TURRUBIATES','1990-08-05','M',null,null,null,null);
-call SP_insert_agente ('MARTHA ALICIA','LEON','CARREÑO','1995-10-03','F',null,null,null,null);
-call SP_insert_agente ('RODOLFO','HERNANDEZ','AVILA','1989-07-21','M',null,null,null,null);
-call SP_insert_agente ('JULIO CESAR','JUAREZ','BORRAYO','1980-01-12','M',null,null,null,null);
-call SP_insert_agente ('JOSE GUADALUPE','LOZANO','VALDEZ','2000-12-23','M',null,null,null,null);
-call SP_insert_agente ('MIGUEL ANGEL','MEDINA','DEL TORO','1990-08-05','M',null,null,null,null);
-call SP_insert_agente ('ALEJANDRO','GUTIERREZ','PEREZ','1995-10-03','M',null,null,null,null);
-call SP_insert_agente ('CARMEN','MIRANDA','ROCHA','2000-12-23','F',null,null,null,null);
+call SP_insert_agente ('ANA CAROLINA','LOPEZ','MARTINEZ', '2000-12-23','Femenino',null);
+call SP_insert_agente ('MANUEL','MENDEZ','HERNANDEZ','1990-08-03','Masculino','6646211878');
+call SP_insert_agente ('MARCO ANTONIO','RAMIREZ','LOZANO','1980-01-12','Masculino',null);
+call SP_insert_agente ('JORGE','CARRILLO','MARTINEZ','1990-08-05','Masculino','6646099823');
+call SP_insert_agente ('JOHANA VANESSA','MAR','SANCHEZ','1989-07-21','Femenino',null);
+call SP_insert_agente ('JESUS ENRIQUE','VAZQUEZ','MARIN','2000-12-23','Masculino','6646075408');
+call SP_insert_agente ('GINA ELIZABETH','LISZT','CORDOBA','1995-10-03','Femenino',null);
+call SP_insert_agente ('PAOLA','MARTINEZ','LARA','1980-01-12','Femenino','6641045520');
+call SP_insert_agente ('MARTHA','CHAVEZ','OCHOA','1989-07-21','Femenino',null);
+call SP_insert_agente ('JOSE ALFONSO','PEREZ','CANTO','1990-08-05','Masculino','6646345289');
+call SP_insert_agente ('FELIPE','LOPEZ','CHAVIRA','1999-07-21','Masculino','6646882150');
+call SP_insert_agente ('VERONICA','FERNANDEZ','ZAMUDIO','2000-12-23','Femenino',null);
+call SP_insert_agente ('SANDRA','GARCIA','BUSTAMANTE','1989-07-21','Femenino','6646231359');
+call SP_insert_agente ('JUAN CARLOS','JARAMILLO','VAZQUEZ','1980-01-12','Masculino','6649060024');
+call SP_insert_agente ('VICTOR ALFONSO','CALDERON','TURRUBIATES','1990-08-05','Masculino','6646834654');
+call SP_insert_agente ('MARTHA ALICIA','LEON','CARREÑO','1995-10-03','Femenino',null);
+call SP_insert_agente ('RODOLFO','HERNANDEZ','AVILA','1989-07-21','Masculino','6646834654');
+call SP_insert_agente ('JULIO CESAR','JUAREZ','BORRAYO','1980-01-12','Masculino','6646073608');
+call SP_insert_agente ('JOSE GUADALUPE','LOZANO','VALDEZ','2000-12-23','Masculino',null);
+call SP_insert_agente ('MIGUEL ANGEL','MEDINA','DEL TORO','1990-08-05','Masculino','6649736631');
+call SP_insert_agente ('ALEJANDRO','GUTIERREZ','PEREZ','1995-10-03','Masculino','6646818243');
+call SP_insert_agente ('CARMEN','MIRANDA','ROCHA','2000-12-23','Femenino',null);
 
 select * from agente;
 
 ########################################### CLIENTES ###################################################################
 # call SP_insert_cliente ('Maria','Peña','Santos','1999-10-05','6657700934','santos_maria','123456','santosM@gmail.com');
-
-call SP_insert_cliente ('MARTINA','ALTAMIRANO','CALDERON','1999-10-05','6647733123',null,'123456','m.altamiro@gmail.com');
-call SP_insert_cliente ('ZULEIMA GABRIELA','ORDOÑES','ALANIS','2000-11-12','6641234567','zuleima05','654321','zuleima@hotmail.com');
-call SP_insert_cliente ('MARISOL','SANCHEZ','PEREZ','1980-01-15','6650099724','marisolS','098765','marisolSP@gmail.com');
-call SP_insert_cliente ('MAGDALENA','GONZALEZ','RODRIGUEZ','1990-05-20',null,'Mgonzales','contrasenia','gonzalesMR@hotmail.com');
-call SP_insert_cliente ('GUADALUPE','RODRIGUEZ','BARTOLO','1990-05-20',null,null,'password','guadalupe@hotmail.com');
-call SP_insert_cliente ('YULETH ARELY','ORTIZ','RODRIGUEZ','1999-10-05','6647733553','Arely15','arely','arelyY@outlook.com');
-call SP_insert_cliente ('JUAN','ESPINOZA','RODRIGUEZ','2000-11-12','6649933123','juan_espinoza','correo','juan.ER@gmail.com');
-call SP_insert_cliente ('SALOME','RIVERA','VAZQUEZ','1980-01-15','6650011724',null,'salome','salome@gmail.com');
-call SP_insert_cliente ('EMMA PAULA','SOLIS','RAMIREZ','1990-05-20',null,'solis_emma','pelicula','emmaPSR@outlook.com');
-call SP_insert_cliente ('DANIEL','TINAJERO','TRISTAN','1980-01-15','6647733133','daniel01','basura','daniel@outlook.com');
-call SP_insert_cliente ('MARIA GUADALUPE','MORENO','CASTILLO','2000-11-12','6651199724',null,'guadalupe','mariaGMc@gmail.com');
-call SP_insert_cliente ('JOSE GUADALUPE','MEDINA','RODRIGUEZ','1999-10-05',null,'jose_guadalupe','constrasenia','joseGuadalupe@outloo.com');
+/*
+in nombre varchar(30),
+	in apPat varchar(20),
+	in apMat varchar(20),
+	in fecNac date,
+    in telefono char(10)
+*/
+call SP_insert_cliente ('MARTINA','ALTAMIRANO','CALDERON','1999-10-05','6647733123');             #(null,'123456','m.altamiro@gmail.com');
+call SP_insert_cliente ('ZULEIMA GABRIELA','ORDOÑES','ALANIS','2000-11-12','6641234567');         #('zuleima05','654321','zuleima@hotmail.com');
+call SP_insert_cliente ('MARISOL','SANCHEZ','PEREZ','1980-01-15','6650099724');                   #('marisolS','098765','marisolSP@gmail.com');
+call SP_insert_cliente ('MAGDALENA','GONZALEZ','RODRIGUEZ','1990-05-20',null);                    #('Mgonzales','contrasenia','gonzalesMR@hotmail.com');
+call SP_insert_cliente ('GUADALUPE','RODRIGUEZ','BARTOLO','1990-05-20',null);                     #(null,'password','guadalupe@hotmail.com');
+call SP_insert_cliente ('YULETH ARELY','ORTIZ','RODRIGUEZ','1999-10-05','6647733553');            #('Arely15','arely','arelyY@outlook.com');
+call SP_insert_cliente ('JUAN','ESPINOZA','RODRIGUEZ','2000-11-12','6649933123');                 #('juan_espinoza','correo','juan.ER@gmail.com');
+call SP_insert_cliente ('SALOME','RIVERA','VAZQUEZ','1980-01-15','6650011724');                   #(null,'salome','salome@gmail.com');
+call SP_insert_cliente ('EMMA PAULA','SOLIS','RAMIREZ','1990-05-20',null);                        #('solis_emma','pelicula','emmaPSR@outlook.com');
+call SP_insert_cliente ('DANIEL','TINAJERO','TRISTAN','1980-01-15','6647733133');                 #('daniel01','basura','daniel@outlook.com');
+call SP_insert_cliente ('MARIA GUADALUPE','MORENO','CASTILLO','2000-11-12','6651199724');         #(null,'guadalupe','mariaGMc@gmail.com');
+call SP_insert_cliente ('JOSE GUADALUPE','MEDINA','RODRIGUEZ','1999-10-05',null);                 #('jose_guadalupe','constrasenia','joseGuadalupe@outloo.com');
 
 select * from cliente;
 select * from usuario;
-select * from telef_clientes;
 
-########################################### Proveedores ###########################################################################
+
+############################################ LUGAR #########################################################
+
+call SP_insert_lugar ('Salón Toscano',
+'Se trata de un cómodo y cálido lugar en el que podrán compartir con sus familiares y 
+allegados. Allí les brindarán los servicios necesarios para que el evento sea 
+perfecto y para que no tengan que preocuparse por nada. Salón Toscano cuenta con 
+una capacidad para 70 personas, en sus instalaciones podrán hacer el montaje que 
+deseen y desarrollar como mejor les parezca la celebración de su evento. 
+El lugar dispone de Estacionamientos y vigilancia privada durante todo su evento.',
+2500.00,70,1,'TIJ','Matamoros','7545','5','22245');
+
+call SP_insert_lugar ('Salón Casa Blanca',
+'Un hermoso salón finamente decorado y con unas instalaciones de alta calidad que ahora les 
+abre sus puertas para que puedan celebrar su evento y que salga perfecto. Las instalaciones 
+de este salón son de excelente calidad y están muy bien acondicionadas para albergar toda 
+clase de eventos sociales. Elegir este salón será un paso hacia el éxito seguro de su enlace. 
+Gracias a su gran extensión el Salón Casa Blanca puede albergar eventos de 100 hasta 500 
+personas garantizando el confort en todo momento.',
+3500.00,500,1,'ESE','Juarez','40',null,'22289');
+
+call SP_insert_lugar ('Belio Restaurante',
+'Belio Restaurante es un lugar mágico para que ustedes celebren con el mar de fondo. La suave 
+brisa marítima será una invitada más a la fiesta y les acompañará durante toda la noche, desde 
+el atardecer en el mar hasta que salen los primeros rayos del sol después de la fiesta. Además 
+del sitio, ustedes van a disfrutar de una auténtica cocina gourmet, ideal para maravillar los 
+paladares de sus invitados. Belio Restaurante es un espacio para sentir una atmósfera diferente, 
+el lugar cuenta con varios salones, terraza y un balcón que da directamente a la playa. 
+Además de la cocina para uso del catering del restaurante.',
+30000.00,200,3,'MXL','San Gabriel','8567',null,'22255');
+
+call SP_insert_lugar ('Puerto Nuevo',
+'Puerto Nuevo Baja, Hotel y Villas es un precioso y exclusivo hotel a la orilla de la playa, 
+que cuenta con grandes vistas, el cual tiene la experiencia de ofrecer sus instalaciones y 
+servicios para su celebración, convirtiendo su evento en algo único e inolvidable. El resort 
+cuenta con 3 salones para eventos, 2 jardines para la ceremonia religiosa o civil, un bar, 
+spa y estacionamiento.',60061.00,600,7,'ESE','San Marcos','1234','18','22345');
+
+call SP_insert_lugar ('Jardín Maui',
+'Por su ambiente encantador y por la elegancia de sus espacios, Jardín Maui es el lugar 
+ideal para sus eventos. En estas instalaciones podrán celebrar tanto un enlace al exterior 
+como en el interior. Por su parte, el equipo del salón pondrá a su disposición sus variados 
+servicios para que su evento resulte exitoso. El compromiso de este lugar unido a sus 
+servicios de calidad les garantizará momentos inolvidables.',
+19000,200,5,'ESE','Av. Londres','1857',null,'22120');
+
+select * from lugar;
+select * from diclugar;
+delete from lugar
+where lugNum=1 or lugNum=2 or lugNum=3 or lugNum=4 or lugNum=5;
+ALTER TABLE lugar AUTO_INCREMENT = 0;
+
+alter table lugar
+modify column lugCosto decimal(12,2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*########################################### Proveedores ###########################################################################
 
 call SP_insert_proveedor('Palacios Diseño',' Palacios Diseño y Decoración cuenta con 55 años de experiencia en el 
 rubro de producción y decoración de eventos, tiempo que avala la excelente calidad de sus servicios. 
@@ -101,7 +195,8 @@ pero ante todo, que con sus preparaciones exquisitas vivan su celebración con e
 estomago lleno y corazón contento. Desde 50 hasta 500 invitados.',
 'https://www.facebook.com/banqueteskrissa/');
 
-select * from proveedor;
+select * from proveedor;*/
 
 /*alter table proveedor
 modify column pro_Sitio varchar(60);*/
+
--- Paradise BD/Trigger_SP.sql
+++ Paradise BD/Trigger_SP.sql
@@ -11,13 +11,9 @@ create procedure SP_insert_agente (
 	in apPat varchar(20),
 	in apMat varchar(20),
 	in fecNac date,
-	in genero char(1),
+	in genero varchar(20),
     
-	in telefono char(10),
-    
-    in userName varchar(30),
-    in contrasenia varchar(30),
-    in correo varchar(30)
+	in telefono char(10)
 ) 
 begin
 	declare edad int;
@@ -31,16 +27,25 @@ begin
 	insert into agente values(matricula,nombre,apPat,apMat,fecNac,edad,genero);
     
     if (telefono is not null) then
-		insert into telef_agentes (tcTelefono,FK_agente)
+		insert into telef_agentes (tgTelefono,FK_agente)
         values (telefono,matricula);
 	end if;
     
-    if (userName is not null and contrasenia is not null and correo is not null) then
-    	insert into usuario (usNombre,usContrasenia,usCorreo,FK_TipoUS,FK_Agente) values
-        (userName,contrasenia,correo,'AGT',matricula);
-	end if;
-
 end //
+DELIMITER ;
+
+# drop procedure SP_insert_userAg
+DELIMITER //
+create procedure SP_insert_userAg(
+	in agente char(7),  #matricula del agente
+    in userName varchar(30),
+    in contrasenia varchar(30),
+    in correo varchar(30)
+)
+begin
+	insert into usuario (usNombre,usContrasenia,usCorreo,FK_TipoUS,FK_Agente) values
+	(userName,contrasenia,correo,'Agente',agente);
+end//
 DELIMITER ;
 
 #call SP_insert_agente('Chapis','Pepogi','Ramirez','2000-12-23','F');
@@ -49,7 +54,7 @@ DELIMITER ;
 
 ########################################### INSERTAR A UN CLIENTE ##################################################
 
-# drop procedure SP_insert_cliente;
+drop procedure SP_insert_cliente;
 
 DELIMITER //
 create procedure SP_insert_cliente (
@@ -57,101 +62,48 @@ create procedure SP_insert_cliente (
 	in apPat varchar(20),
 	in apMat varchar(20),
 	in fecNac date,
+    in telefono char(10)
     
-    in telefono char(10),
-    
-    in userName varchar(30),
+    /*in userName varchar(30),
     in contrasenia varchar(30),
-    in correo varchar(30)
+    in correo varchar(30)*/
 ) 
 begin
     declare edad int; 
 	select timestampdiff(year,fecNac,curdate())  into edad;
     
-	insert into cliente (cliNombre,cliApPat,cliApMat,cliFecNac,cliEdad)values(
-	nombre,apPat,apMat,fecNac,edad);
-    
-    if (telefono is not null) then
-		insert into telef_clientes (tcTelefono,FK_cliente)
-        values (telefono,(select cliNum from cliente where cliNombre=nombre and cliApPat=apPat and cliApMat=apMat and cliFecNac=fecNac));
-	end if;
-    
-    if (userName is null) then
-    	insert into usuario (usNombre,usContrasenia,usCorreo,FK_TipoUS,FK_Cliente)
-        values((concat((select count(cliNum) from cliente where cliNombre=nombre),'_',nombre,'_',apPat)),contrasenia,correo,'CLI',
-        (select cliNum from cliente where cliNombre=nombre and cliApPat=apPat and cliApMat=apMat and cliFecNac=fecNac));
-	else 
-		insert into usuario (usNombre,usContrasenia,usCorreo,FK_TipoUS,FK_Cliente)
-        values (userName,contrasenia,correo,'CLI',
-        (select cliNum from cliente where cliNombre=nombre and cliApPat=apPat and cliApMat=apMat and cliFecNac=fecNac));
-	end if;
-    
-end//
-DELIMITER ;
- 
-
-# call SP_insert_cliente ('Yoel','Mendez','Arriaga','1997-08-10',null,null,'123456','arriaga@gmail.com');
-# call SP_insert_cliente ('Kevin','Ortiz','Rieta','2001-12-02','6649977543',null,'123456','rieta@gmail.com');
-# call SP_insert_cliente ('Maria','Peña','Santos','1999-10-05',null,'santos_maria','123456','santosM@gmail.com');
-
-#  delete from usuario where usNum=8 or usNum=9 or usNum=17;
-# delete from cliente where cliNum=14 or cliNum=15 or cliNum=17;
-# delete from telef_clientes where tcNum = 4;
-
-# select * from usuario;
-# select * from cliente;
-# select * from telef_clientes;
-
-
-########################################### SP PARA INSERT PROVEEDORES ##############################################
-
-# drop procedure SP_insert_proveedor;
-
-DELIMITER //
-create procedure SP_insert_proveedor (
-    in nombre varchar(50),
-	in descripcion text,
-	in sitio varchar(60)
-) 
-begin
-	declare codigo char(5);
-	set codigo=upper(concat(substr(nombre,1,2),substr(nombre,char_length(nombre),2),'0'));
-    
-    insert into proveedor values (codigo,nombre,descripcion,sitio);
+	insert into cliente (cliNombre,cliApPat,cliApMat,cliFecNac,cliEdad,cliTelefono)values(
+	nombre,apPat,apMat,fecNac,edad,telefono);
     
 end//
 DELIMITER ;
 
-
-##########################################################################################################
-#insert usuarios agentes
-
 DELIMITER //
-create procedure SP_insert_user_agente(
-	in agente char(7),  #matricula del agente
+create procedure SP_insert_userCli(
+	in cliente char(7),  #no.cliente
     in userName varchar(30),
     in contrasenia varchar(30),
     in correo varchar(30)
 )
 begin
 	insert into usuario (usNombre,usContrasenia,usCorreo,FK_TipoUS,FK_Agente) values
-	(userName,contrasenia,correo,'AGT',agente);
+	(userName,contrasenia,correo,'Cliente',cliente);
 end//
 DELIMITER ;
 
 ########################################### INSERTAR UN LUGAR ####################################################
 
-drop procedure SP_insert_lugar;
+# drop procedure SP_insert_lugar;
 
 DELIMITER //
 create procedure SP_insert_lugar(
 	in nombre varchar(30),
     in descripcion text,
-    in costo decimal(6,2),
+    in costo decimal(12,2),
     in capacidad int,
     in tipoLug int,
     in municipio char(3),
-    in proveedor char(5),
+    # in proveedor char(5),
     
     in calle varchar(40),
     in numInt varchar(25),
@@ -159,26 +111,23 @@ create procedure SP_insert_lugar(
     in cp char(5)
 )
 begin
-	declare codigo char(5);
-    set codigo=upper(concat(substr(nombre,1,1),substr(nombre,char_length(nombre),1),tipoLug,substr(municipio,1,2)));
-    
+	
     if (calle is not null and numInt is not null and cp is not null) then
 		
-        insert into diclugar(dlCalle,dlNumInterior,dlNumExterior,dlCP) values
-        (calle,numInt,numExt,cp);
+        insert into lugar(lugNombre,lugDescripcion,lugCosto,lugCapacidad,FK_TipoL,FK_Municipio) values
+        (nombre,descripcion,costo,capacidad,tipoLug,municipio);
         
-        insert into lugar values
-        (codigo,nombre,descripcion,costo,capacidad,tipoLug,
-        (select dlNum from diclugar where dlCalle=calle and dlNumInterior=numInt and dlCP=cp),
-        municipio,proveedor);
+        insert into diclugar values ((select lugNum from lugar order by lugNum desc limit 1),
+        calle,numInt,numExt,cp);
+        
     else
-		insert into lugar values
-        (codigo,nombre,descripcion,costo,capacidad,tipoLug,null,municipio,proveedor);
+		insert into lugar(lugNombre,lugDescripcion,lugCosto,lugCapacidad,FK_TipoL,FK_Municipio) values
+        (nombre,descripcion,costo,capacidad,tipoLug,municipio);
     end if;
     
 end//
 DELIMITER ;
 
-# call SP_insert_lugar ('Salón Toscano',null,250,70,1,'TIJ','BAA0','Matamoros','7545','5','22245');
-# select * from lugar;
-# select * from diclugar;
+
+
+
