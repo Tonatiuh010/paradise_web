a7b5027dbca108dab09afef06409d4b487b759da
Denisse-Arreola <arxie.denisse@gmail.com>
2020-10-29 19:05:27 -0700

    Base de datos actualizada :v

--- Paradise BD/Constraint.sql
+++ Paradise BD/Constraint.sql
@@ -6,11 +6,11 @@ add constraint UQ_agNombre_Completo unique(agNombre,agApPat,agApMat);
 alter table cliente
 add constraint UQ_cliNombre_Completo unique(cliNombre,cliApPat,cliApMat);
 
-alter table cliente
-drop constraint UQ_cliNombre_Completo;
+# alter table cliente
+# drop constraint UQ_cliNombre_Completo;
 
-alter table diclugar
-alter dlNumExterior set default 'n/a';
+# alter table diclugar
+# alter dlNumExterior set default 'n/a';
 
 alter table cliente
 ADD CONSTRAINT CK_cliente_edad CHECK (cliEdad>=18);
@@ -19,4 +19,10 @@ alter table agente
 add constraint CK_agente_edad check(agEdad>=18);
 
 alter table agente
-add constraint CK_agente_genero check(agGenero = 'Masculino' or 'Femenino');
+add constraint CK_agente_genero check(agGenero = 'Masculino' or agGenero= 'Femenino');
+
+# alter table agente
+# drop constraint CK_agente_genero;
+
+alter table usuario
+add constraint CK_usuario_tipoUS check(usTipoUS = 'Agente' or usTipoUS = 'Cliente' or usTipoUS = 'Administrador');
--- Paradise BD/Insert.sql
+++ Paradise BD/Insert.sql
@@ -18,54 +18,86 @@ select * from espacio;
 
 
 ########################################### AGENTES ###################################################################
-# call SP_insert_agente('Chapis','Pepogi','Ramirez','2000-12-23','F');
+# call SP_insert_userAg ('jose_guadalupe','constrasenia','joseGuadalupe@outloo.com');
 
-call SP_insert_agente ('ANA CAROLINA','LOPEZ','MARTINEZ', '2000-12-23','Femenino',null);
-call SP_insert_agente ('MANUEL','MENDEZ','HERNANDEZ','1990-08-03','Masculino','6646211878');
-call SP_insert_agente ('MARCO ANTONIO','RAMIREZ','LOZANO','1980-01-12','Masculino',null);
-call SP_insert_agente ('JORGE','CARRILLO','MARTINEZ','1990-08-05','Masculino','6646099823');
-call SP_insert_agente ('JOHANA VANESSA','MAR','SANCHEZ','1989-07-21','Femenino',null);
-call SP_insert_agente ('JESUS ENRIQUE','VAZQUEZ','MARIN','2000-12-23','Masculino','6646075408');
-call SP_insert_agente ('GINA ELIZABETH','LISZT','CORDOBA','1995-10-03','Femenino',null);
-call SP_insert_agente ('PAOLA','MARTINEZ','LARA','1980-01-12','Femenino','6641045520');
-call SP_insert_agente ('MARTHA','CHAVEZ','OCHOA','1989-07-21','Femenino',null);
-call SP_insert_agente ('JOSE ALFONSO','PEREZ','CANTO','1990-08-05','Masculino','6646345289');
-call SP_insert_agente ('FELIPE','LOPEZ','CHAVIRA','1999-07-21','Masculino','6646882150');
-call SP_insert_agente ('VERONICA','FERNANDEZ','ZAMUDIO','2000-12-23','Femenino',null);
-call SP_insert_agente ('SANDRA','GARCIA','BUSTAMANTE','1989-07-21','Femenino','6646231359');
-call SP_insert_agente ('JUAN CARLOS','JARAMILLO','VAZQUEZ','1980-01-12','Masculino','6649060024');
-call SP_insert_agente ('VICTOR ALFONSO','CALDERON','TURRUBIATES','1990-08-05','Masculino','6646834654');
-call SP_insert_agente ('MARTHA ALICIA','LEON','CARREÑO','1995-10-03','Femenino',null);
+call SP_insert_userAg ('user1','St@rt123','iabarcae@yahoo.es');
+call SP_insert_userAg ('admin','P@ssw0rd','maeillanes@hotmail.com');
+call SP_insert_userAg ('alex','bl4ck4ndwhite','osabarca@hotmail.com'); 
+call SP_insert_userAg ('pos','administrator','cabrigo@garmendia.cl'); 
+call SP_insert_userAg ('demo','.......','Sb.nashxo.sk8@hotmail.com'); 
+call SP_insert_userAg ('sql','123456','fran.afull@live.cl'); 
+call SP_insert_userAg (null,'654321','carlosaguileram@hotmail.com'); 	 
+call SP_insert_userAg (null,'password','ikis_rojo@hotmail.com'); 
+call SP_insert_userAg (null,'constraseña','daniela_aguilera_m500@hotmail.com'); 
+call SP_insert_userAg (null,'Juana','vizkala@hotmail.com');	 
+call SP_insert_userAg (null,'123456','alexus3@hotmail.com');	
+call SP_insert_userAg (null,'654321','capitanaguilera@hotmail.com');	 
+call SP_insert_userAg (null,'098765','apalamosg@hotmail.com');	 
+call SP_insert_userAg (null,'567890','niikhox__@hotmail.com');
+call SP_insert_userAg (null,'345678','luuuuuuci@hotmail.com'); 
+call SP_insert_userAg (null,'876543','kristian_siempre_azul@hotmail.com');
+
+select * from usuario;
+
+#call SP_insert_agente ('JOSE GUADALUPE','LOZANO','VALDEZ','2000-12-23','Masculino','joseGuadalupe@outloo.com','6645577934');
+
+call SP_insert_agente ('ANA CAROLINA','LOPEZ','MARTINEZ', '2000-12-23','Femenino','iabarcae@yahoo.es',null);
+call SP_insert_agente ('MANUEL','MENDEZ','HERNANDEZ','1990-08-03','Masculino','maeillanes@hotmail.com','6646211878');
+call SP_insert_agente ('MARCO ANTONIO','RAMIREZ','LOZANO','1980-01-12','Masculino','osabarca@hotmail.com',null);
+call SP_insert_agente ('JORGE','CARRILLO','MARTINEZ','1990-08-05','Masculino','cabrigo@garmendia.cl','6646099823');
+call SP_insert_agente ('JOHANA VANESSA','MAR','SANCHEZ','1989-07-21','Femenino','Sb.nashxo.sk8@hotmail.com',null);
+call SP_insert_agente ('JESUS ENRIQUE','VAZQUEZ','MARIN','2000-12-23','Masculino','fran.afull@live.cl','6646075408');
+call SP_insert_agente ('GINA ELIZABETH','LISZT','CORDOBA','1995-10-03','Femenino','carlosaguileram@hotmail.com',null);
+call SP_insert_agente ('PAOLA','MARTINEZ','LARA','1980-01-12','Femenino','ikis_rojo@hotmail.com','6641045520');
+call SP_insert_agente ('MARTHA','CHAVEZ','OCHOA','1989-07-21','Femenino','daniela_aguilera_m500@hotmail.com',null);
+call SP_insert_agente ('JOSE ALFONSO','PEREZ','CANTO','1990-08-05','Masculino','vizkala@hotmail.com','6646345289');
+call SP_insert_agente ('FELIPE','LOPEZ','CHAVIRA','1999-07-21','Masculino','alexus3@hotmail.com','6646882150');
+call SP_insert_agente ('VERONICA','FERNANDEZ','ZAMUDIO','2000-12-23','Femenino','capitanaguilera@hotmail.com',null);
+call SP_insert_agente ('SANDRA','GARCIA','BUSTAMANTE','1989-07-21','Femenino','apalamosg@hotmail.com','6646231359');
+call SP_insert_agente ('JUAN CARLOS','JARAMILLO','VAZQUEZ','1980-01-12','Masculino','niikhox__@hotmail.com','6649060024');
+call SP_insert_agente ('VICTOR ALFONSO','CALDERON','TURRUBIATES','1990-08-05','Masculino','luuuuuuci@hotmail.com','6646834654');
+call SP_insert_agente ('MARTHA ALICIA','LEON','CARREÑO','1995-10-03','Femenino','kristian_siempre_azul@hotmail.com',null);
+
+/*
 call SP_insert_agente ('RODOLFO','HERNANDEZ','AVILA','1989-07-21','Masculino','6646834654');
 call SP_insert_agente ('JULIO CESAR','JUAREZ','BORRAYO','1980-01-12','Masculino','6646073608');
 call SP_insert_agente ('JOSE GUADALUPE','LOZANO','VALDEZ','2000-12-23','Masculino',null);
 call SP_insert_agente ('MIGUEL ANGEL','MEDINA','DEL TORO','1990-08-05','Masculino','6649736631');
 call SP_insert_agente ('ALEJANDRO','GUTIERREZ','PEREZ','1995-10-03','Masculino','6646818243');
 call SP_insert_agente ('CARMEN','MIRANDA','ROCHA','2000-12-23','Femenino',null);
-
+*/
 select * from agente;
 
 ########################################### CLIENTES ###################################################################
-# call SP_insert_cliente ('Maria','Peña','Santos','1999-10-05','6657700934','santos_maria','123456','santosM@gmail.com');
-/*
-in nombre varchar(30),
-	in apPat varchar(20),
-	in apMat varchar(20),
-	in fecNac date,
-    in telefono char(10)
-*/
-call SP_insert_cliente ('MARTINA','ALTAMIRANO','CALDERON','1999-10-05','6647733123');             #(null,'123456','m.altamiro@gmail.com');
-call SP_insert_cliente ('ZULEIMA GABRIELA','ORDOÑES','ALANIS','2000-11-12','6641234567');         #('zuleima05','654321','zuleima@hotmail.com');
-call SP_insert_cliente ('MARISOL','SANCHEZ','PEREZ','1980-01-15','6650099724');                   #('marisolS','098765','marisolSP@gmail.com');
-call SP_insert_cliente ('MAGDALENA','GONZALEZ','RODRIGUEZ','1990-05-20',null);                    #('Mgonzales','contrasenia','gonzalesMR@hotmail.com');
-call SP_insert_cliente ('GUADALUPE','RODRIGUEZ','BARTOLO','1990-05-20',null);                     #(null,'password','guadalupe@hotmail.com');
-call SP_insert_cliente ('YULETH ARELY','ORTIZ','RODRIGUEZ','1999-10-05','6647733553');            #('Arely15','arely','arelyY@outlook.com');
-call SP_insert_cliente ('JUAN','ESPINOZA','RODRIGUEZ','2000-11-12','6649933123');                 #('juan_espinoza','correo','juan.ER@gmail.com');
-call SP_insert_cliente ('SALOME','RIVERA','VAZQUEZ','1980-01-15','6650011724');                   #(null,'salome','salome@gmail.com');
-call SP_insert_cliente ('EMMA PAULA','SOLIS','RAMIREZ','1990-05-20',null);                        #('solis_emma','pelicula','emmaPSR@outlook.com');
-call SP_insert_cliente ('DANIEL','TINAJERO','TRISTAN','1980-01-15','6647733133');                 #('daniel01','basura','daniel@outlook.com');
-call SP_insert_cliente ('MARIA GUADALUPE','MORENO','CASTILLO','2000-11-12','6651199724');         #(null,'guadalupe','mariaGMc@gmail.com');
-call SP_insert_cliente ('JOSE GUADALUPE','MEDINA','RODRIGUEZ','1999-10-05',null);                 #('jose_guadalupe','constrasenia','joseGuadalupe@outloo.com');
+# call SP_insert_userCli ('daniel01','basura','daniel@outlook.com');
+
+call SP_insert_userCli (null,'123456','m.altamiro@gmail.com');
+call SP_insert_userCli ('zuleima05','654321','zuleima@hotmail.com');
+call SP_insert_userCli ('marisolS','098765','marisolSP@gmail.com');
+call SP_insert_userCli ('Mgonzales','contrasenia','gonzalesMR@hotmail.com');
+call SP_insert_userCli (null,'password','guadalupe@hotmail.com');
+call SP_insert_userCli ('Arely15','arely','arelyY@outlook.com');
+call SP_insert_userCli ('juan_espinoza','correo','juan.ER@gmail.com');
+call SP_insert_userCli (null,'salome','salome@gmail.com');
+call SP_insert_userCli ('solis_emma','pelicula','emmaPSR@outlook.com');
+call SP_insert_userCli ('daniel01','basura','daniel@outlook.com');
+call SP_insert_userCli (null,'guadalupe','mariaGMc@gmail.com');
+call SP_insert_userCli ('jose_guadalupe','constrasenia','joseGuadalupe@outloo.com');
+
+# call SP_insert_cliente ('DANIEL','TINAJERO','TRISTAN','1980-01-15','6647733133','daniel@outlook.com');
+
+call SP_insert_cliente ('MARTINA','ALTAMIRANO','CALDERON','1999-10-05','6647733123','m.altamiro@gmail.com');             
+call SP_insert_cliente ('ZULEIMA GABRIELA','ORDOÑES','ALANIS','2000-11-12','6641234567','zuleima@hotmail.com');         
+call SP_insert_cliente ('MARISOL','SANCHEZ','PEREZ','1980-01-15','6650099724','marisolSP@gmail.com');                   
+call SP_insert_cliente ('MAGDALENA','GONZALEZ','RODRIGUEZ','1990-05-20',null,'gonzalesMR@hotmail.com');                    
+call SP_insert_cliente ('GUADALUPE','RODRIGUEZ','BARTOLO','1990-05-20',null,'guadalupe@hotmail.com');                     
+call SP_insert_cliente ('YULETH ARELY','ORTIZ','RODRIGUEZ','1999-10-05','6647733553','arelyY@outlook.com');            
+call SP_insert_cliente ('JUAN','ESPINOZA','RODRIGUEZ','2000-11-12','6649933123','juan.ER@gmail.com');                 
+call SP_insert_cliente ('SALOME','RIVERA','VAZQUEZ','1980-01-15','6650011724','salome@gmail.com');                   
+call SP_insert_cliente ('EMMA PAULA','SOLIS','RAMIREZ','1990-05-20',null,'emmaPSR@outlook.com');                        
+call SP_insert_cliente ('DANIEL','TINAJERO','TRISTAN','1980-01-15','6647733133','daniel@outlook.com');                 
+call SP_insert_cliente ('MARIA GUADALUPE','MORENO','CASTILLO','2000-11-12','6651199724','mariaGMc@gmail.com');         
+call SP_insert_cliente ('JOSE GUADALUPE','MEDINA','RODRIGUEZ','1999-10-05',null,'joseGuadalupe@outloo.com');                 
 
 select * from cliente;
 select * from usuario;
@@ -80,7 +112,7 @@ perfecto y para que no tengan que preocuparse por nada. Salón Toscano cuenta co
 una capacidad para 70 personas, en sus instalaciones podrán hacer el montaje que 
 deseen y desarrollar como mejor les parezca la celebración de su evento. 
 El lugar dispone de Estacionamientos y vigilancia privada durante todo su evento.',
-2500.00,70,1,'TIJ','Matamoros','7545','5','22245');
+2500.00,70,1,'Matamoros','7545','5','22245','TIJ');
 
 call SP_insert_lugar ('Salón Casa Blanca',
 'Un hermoso salón finamente decorado y con unas instalaciones de alta calidad que ahora les 
@@ -89,7 +121,7 @@ de este salón son de excelente calidad y están muy bien acondicionadas para al
 clase de eventos sociales. Elegir este salón será un paso hacia el éxito seguro de su enlace. 
 Gracias a su gran extensión el Salón Casa Blanca puede albergar eventos de 100 hasta 500 
 personas garantizando el confort en todo momento.',
-3500.00,500,1,'ESE','Juarez','40',null,'22289');
+3500.00,500,1,'Juarez','40',null,'22289','ESE');
 
 call SP_insert_lugar ('Belio Restaurante',
 'Belio Restaurante es un lugar mágico para que ustedes celebren con el mar de fondo. La suave 
@@ -99,14 +131,14 @@ del sitio, ustedes van a disfrutar de una auténtica cocina gourmet, ideal para 
 paladares de sus invitados. Belio Restaurante es un espacio para sentir una atmósfera diferente, 
 el lugar cuenta con varios salones, terraza y un balcón que da directamente a la playa. 
 Además de la cocina para uso del catering del restaurante.',
-30000.00,200,3,'MXL','San Gabriel','8567',null,'22255');
+30000.00,200,3,'San Gabriel','8567',null,'22255','MXL');
 
 call SP_insert_lugar ('Puerto Nuevo',
 'Puerto Nuevo Baja, Hotel y Villas es un precioso y exclusivo hotel a la orilla de la playa, 
 que cuenta con grandes vistas, el cual tiene la experiencia de ofrecer sus instalaciones y 
 servicios para su celebración, convirtiendo su evento en algo único e inolvidable. El resort 
 cuenta con 3 salones para eventos, 2 jardines para la ceremonia religiosa o civil, un bar, 
-spa y estacionamiento.',60061.00,600,7,'ESE','San Marcos','1234','18','22345');
+spa y estacionamiento.',60061.00,600,7,'San Marcos','1234','18','22345','ESE');
 
 call SP_insert_lugar ('Jardín Maui',
 'Por su ambiente encantador y por la elegancia de sus espacios, Jardín Maui es el lugar 
@@ -114,7 +146,17 @@ ideal para sus eventos. En estas instalaciones podrán celebrar tanto un enlace 
 como en el interior. Por su parte, el equipo del salón pondrá a su disposición sus variados 
 servicios para que su evento resulte exitoso. El compromiso de este lugar unido a sus 
 servicios de calidad les garantizará momentos inolvidables.',
-19000,200,5,'ESE','Av. Londres','1857',null,'22120');
+19000,200,5,'Av. Londres','1857',null,'22120','ESE');
+
+#---------------- insert de prueba sin domiclio ---------------------------------------------
+call SP_insert_lugar ('Cocina Rusa Familia Samarin',
+'Cocina Rusa Familia Samarin se encuentra en la encantadora ciudad de Ensenada. Es un restaurante 
+con carácter, de impresionantes espacios y sabores y un estilo vibrante que cautivará a todos los 
+invitados. Se sentirán como en casa, ya que el objetivo del personal del lugar es conseguir su 
+máxima satisfacción. Con comodidad podrán recibir varios comensales sin problemas. Tienen varias 
+áreas para su fiesta, cada una con un encanto propio que llenará de magia su evento. 
+Disfruten de la velada junto a sus seres queridos o amigos en las instalaciones que les ofrecen.',
+20000,400,3,null,null,null,null,null);
 
 select * from lugar;
 select * from diclugar;
@@ -128,7 +170,7 @@ select * from diclugar;
 # ALTER TABLE usuario AUTO_INCREMENT = 0;
 
 ########################################### LUGAR ESPACIO ######################################################
-insert into lugespacio (lg_NumEspacio,lg_NumLugar) values
+insert into lugespacio (lg_NumEspacio,lg_NumLugar) values 
 # Lugar 1 Salón Toscano
 (2,1),(3,1),(4,1),
 # Lugar 2 Salón Casa Blanca
@@ -138,7 +180,9 @@ insert into lugespacio (lg_NumEspacio,lg_NumLugar) values
 # Lugar 4 Puerto Nuevo 'Playa'
 (10,4),(7,4),(3,4),(11,4),
 # Lugar 5 Jardín Maui
-(1,5),(11,5);
+(1,5),(11,5),
+# Lugar 6 Cocina Rusa
+(4,6),(5,6),(8,6);
 
 select * from lugar;
 select * from espacio;
